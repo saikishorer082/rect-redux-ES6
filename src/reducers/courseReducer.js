cd4042061055332b4884cc1008967239269ccd99
saikishorer082 <saikishorer082@gmail.com>
2018-07-22 15:07:54 -0400

    Async in Redux

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,26 +1,10 @@
+import * as types from '../actions/actionTypes';
 export default function courseReducer(state = [], action) {
     switch(action.type) {
-        case 'CREATE_COURSE':
-          // state.push(action.course);
-          // return state;  // this is wrong code because in redux state is immutable.
-          return [...state, 
-            Object.assign({}, action.course)
-          ];
+        case types.LOAD_COURSES_SUCCESS:
+          return action.courses;
 
         default: 
             return state;
     }
 }
-// export default courseReducer((state = [], action) => {
-//     switch(action.type) {
-//         case 'CREATE_COURSE':
-//           // state.push(action.course);
-//           // return state;  // this is wrong code because in redux state is immutable.
-//           return [...state, 
-//             Object.assign({}, action.course)
-//           ];
-
-//         default: 
-//             return state;
-//     }
-// })
